4d0e75a3e43fa02e3e6c28bc6291b6b651ce4f59
Степан-Богдан Поліщук <bohdanpolishchuk@gmail.com>
2019-03-17 22:51:18 +0200

    Routing 115

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -6,7 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
-
+  users = [
+    {
+      id: 1,
+      name: 'Viktor'
+    }, {
+      id: 2,
+      name: 'Ann'
+    }, {
+      id: 3,
+      name: 'Ben'
+    }
+  ];
   constructor() { }
 
   ngOnInit() {
