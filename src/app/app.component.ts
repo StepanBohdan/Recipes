c52a492097bdea55dd4dc0d43718afcebeab7705
Bohdan <bohdanpolishchuk@gmail.com>
2019-03-20 16:37:00 +0200

    start forms 165

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm }    from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  suggestUserName() {
+    const suggestedName = 'Suggested';
+  }
 
+  onSubmit(form: NgForm) {
+    console.log(form);
+  }
 }
 
