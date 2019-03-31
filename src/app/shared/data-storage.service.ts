4fa358df32e2ac4c18174571ac5ddad0f14dfbce
Степан-Богдан Поліщук <bohdanpolishchuk@gmail.com>
2019-03-31 15:06:04 +0300

    interceptor stuff 285

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,13 +1,13 @@
-import { Injectable }    from '@angular/core';
-import { HttpClient }    from '@angular/common/http';
-import { RecipeService } from '../recipes/recipe.service';
-import { Recipe }        from '../recipes/recipe.model';
-import { map }           from 'rxjs/operators';
-import { AuthService }   from '../auth/auth.service';
+import { Injectable }                                       from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams, HttpRequest } from '@angular/common/http';
+import { RecipeService }                                    from '../recipes/recipe.service';
+import { Recipe }                                           from '../recipes/recipe.model';
+import { map }                                              from 'rxjs/operators';
+import { AuthService }                                      from '../auth/auth.service';
 
 @Injectable()
 export class DataStorageService {
-  private recipeStorageUrl = 'https://recipe-b90c5.firebaseio.com/';
+  private recipeStorageUrl = 'https://recipe-b90c5.firebaseio.com/recipes.json';
 
   constructor(private httpClient: HttpClient,
               private recipeService: RecipeService,
@@ -15,13 +15,25 @@ export class DataStorageService {
 
   storeRecipes() {
     const token = this.authServise.getToken();
-    return this.httpClient.put(`${this.recipeStorageUrl}/` + 'recipes.json?auth=' + token,
-      this.recipeService.getRecipes());
+    // const headers = new HttpHeaders().set('Authorization', 'Recipe value');
+
+    // return this.httpClient.put(`${this.recipeStorageUrl}/`,
+    //   this.recipeService.getRecipes(), {
+    //   observe: 'body',
+    //     params: new HttpParams().set('auth', token)
+    //     // headers: headers
+    //   });
+    const req = new HttpRequest('PUT', `${this.recipeStorageUrl}/`, this.recipeService.getRecipes(),
+      {reportProgress: true, params: new HttpParams().set('auth', token)
+      })
+    return this.httpClient.request(req);
   }
 
   getRecipes() {
     const token = this.authServise.getToken();
-    this.httpClient.get<Recipe[]>(`${this.recipeStorageUrl}/` + 'recipes.json?auth=' + token)
+    this.httpClient.get<Recipe[]>(`${this.recipeStorageUrl}/`, {
+      params: new HttpParams().set('auth', token)
+    })
       .pipe(
         map((recipes) => {
           for (const recipe of recipes) {
