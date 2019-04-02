9c41b16ccf91f318bd1c54ed8b0deb82b09ae466
Bohdan <bohdanpolishchuk@gmail.com>
2019-04-02 14:54:54 +0300

    signin 314

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm }            from '@angular/forms';
+import { Store }        from '@ngrx/store';
 
 import * as fromApp     from '../../store/app.reducers';
 import * as AuthActions from '../store/auth.actions';
-import { Store }        from '@ngrx/store';
 
 @Component({
   selector: 'app-signup',
@@ -20,7 +20,7 @@ export class SignupComponent implements OnInit {
   onSignup(form: NgForm) {
     const email = form.value.email;
     const password = form.value.password;
-    this.store.dispatch(new AuthActions.TrySignup({username: email, password}));
+    this.store.dispatch(new AuthActions.TrySignup({username: email, password: password}));
   }
 
 }
--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -1,13 +1,56 @@
-import { Actions, Effect } from '@ngrx/effects';
-import { Injectable }      from '@angular/core';
+import { Injectable }              from '@angular/core';
+import { Actions, Effect, ofType } from '@ngrx/effects';
 
-import * as AuthActions from './auth.actions';
+import * as AuthActions             from './auth.actions';
+import { map, mergeMap, switchMap } from "rxjs/operators";
+import { from }                     from "rxjs";
+import * as firebase from 'firebase';
 
 @Injectable()
 export class AuthEffects {
   @Effect()
-  authSignup = this.actions$;
-    // .ofType(AuthActions.TRY_SIGNUP);
+  authSignup = this.actions$
+    .pipe(
+      ofType(AuthActions.TRY_SIGNUP),
+      map((action: AuthActions.TrySignup) => {
+        return action.payload;
+      }),
+      switchMap((authData: {username: string, password: string }) => {
+        return from(firebase.auth().createUserWithEmailAndPassword(authData.username, authData.password));
+      }),
+      switchMap(() => {
+        return from(firebase.auth().currentUser.getIdToken());
+      }),
+      mergeMap((token: string) => {
+        return [{
+          type: AuthActions.SIGNUP
+        }, {
+          type: AuthActions.SET_TOKEN,
+          payload: token
+        }]
+      }));
 
-  constructor(private actions$: Actions) {}
+  @Effect()
+  authSignin = this.actions$
+    .pipe(
+      ofType(AuthActions.TRY_SIGNIN),
+      map((action: AuthActions.TrySignin) => {
+        return action.payload;
+      }),
+      switchMap((authData: {username: string, password: string }) => {
+        return from(firebase.auth().signInWithEmailAndPassword(authData.username, authData.password));
+      }),
+      switchMap(() => {
+        return from(firebase.auth().currentUser.getIdToken());
+      }),
+      mergeMap((token: string) => {
+        return [{
+          type: AuthActions.SIGNIN
+        }, {
+          type: AuthActions.SET_TOKEN,
+          payload: token
+        }]
+      }));
+
+      constructor(private actions$: Actions) {}
 }
